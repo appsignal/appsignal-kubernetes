36aa4237475fe51994dab33e8f30e5b80f9278fc
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-26 12:54:58 +0100

    Add example post to metrics endpoint
    To send metrics to the endpoint, set the ENDPOINT environment
    variable (including the AppSignal frontend key) and call cargo run:
    
        ENDPOINT=Https://appsignal-endpoint.net/metrics/json?api_key=00000000-0000-0000-0000-000000000000 cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::time::Duration;
+use reqwest::Client;
+use serde_json::json;
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let endpoint = env::var("ENDPOINT").unwrap_or("unknown".to_string());
+    let client = Client::builder().timeout(Duration::from_secs(30)).build()?;
+
+    let json = json!({
+        "name": "cpu_nano_cores",
+        "metricType": "gauge",
+        "value": 184417755,
+        "tags": {"hostname": "minikube"}
+    }).to_string();
+
+    let res = client
+        .post(&endpoint)
+        .body(json.as_str().to_owned())
+        .send()
+        .await?;
+
+    println!("Done: {:?}", res);
+
+    Ok(())
 }
