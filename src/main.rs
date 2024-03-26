214e7db095a2a97ad219442c146a89e2e40c9079
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-26 15:02:11 +0100

    Send more metrics
    Use a hardcoded list of metrics to extract from the API and send to AppSignal.

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,135 @@ async fn main() -> Result<(), Error> {
     let kube_request = Request::get(url).body(Default::default())?;
     let kube_response = kube_client.request::<serde_json::Value>(kube_request).await?;
 
-    let json = json!([
-        {
-            "name": "cpu_nano_cores",
-            "metricType": "gauge",
-            "value": kube_response["node"]["cpu"]["usageNanoCores"],
-            "tags": {"hostname": name}
-        }
-    ]).to_string();
+        let json = json!([
+            {
+                "name": "node_cpu_usage_nano_cores",
+                "metricType": "gauge",
+                "value": kube_response["node"]["cpu"]["usageNanoCores"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_cpu_usage_core_nano_seconds",
+                "metricType": "gauge",
+                "value": kube_response["node"]["cpu"]["usageCoreNanoSeconds"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_memory_usage_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["memory"]["usageBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_memory_working_set_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["memory"]["workingSetBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_memory_rss_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["memory"]["rssBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_memory_page_faults",
+                "metricType": "gauge",
+                "value": kube_response["node"]["memory"]["pageFaults"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_memory_major_page_faults",
+                "metricType": "gauge",
+                "value": kube_response["node"]["memory"]["majorPageFaults"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_network_rx_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["network"]["rxBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_network_rx_errors",
+                "metricType": "gauge",
+                "value": kube_response["node"]["network"]["rxErrors"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_network_tx_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["network"]["txBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_network_tx_errors",
+                "metricType": "gauge",
+                "value": kube_response["node"]["network"]["txErrors"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_fs_available_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["fs"]["availableBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_s_capacity_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["fs"]["capacityBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_fs_used_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["fs"]["usedBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_fs_inodes_free",
+                "metricType": "gauge",
+                "value": kube_response["node"]["fs"]["inodesFree"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_fs_inodes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["fs"]["inodes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_fs_inodes_used",
+                "metricType": "gauge",
+                "value": kube_response["node"]["fs"]["inodesUsed"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_rlimit_maxpid",
+                "metricType": "gauge",
+                "value": kube_response["node"]["rlimit"]["maxpid"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_rlimit_curproc",
+                "metricType": "gauge",
+                "value": kube_response["node"]["rlimit"]["curproc"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_swap_available_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["swap"]["swapAvailableBytes"],
+                "tags": {"node": name}
+            },
+            {
+                "name": "node_swap_usage_bytes",
+                "metricType": "gauge",
+                "value": kube_response["node"]["swap"]["swapUsageBytes"],
+                "tags": {"node": name}
+            }
+        ])
+        .to_string();
 
     let endpoint = env::var("ENDPOINT").unwrap_or("unknown".to_string());
     let reqwest_client = Client::builder().timeout(Duration::from_secs(30)).build()?;
