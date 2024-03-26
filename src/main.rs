51dec941b9e2393e4378ece97a3db6552149d5ff
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-03-26 13:49:08 +0100

    Send actual usageNanoCores value
    The usageNanoCores is extracted from the Kubernetes API, it's assumed
    that a node named "minikube" is running for it to work.

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,38 @@ use std::env;
 use std::time::Duration;
 use reqwest::Client;
 use serde_json::json;
+use http::Request;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let kube_client = kube::Client::try_default().await?;
+    let name = "minikube";
+    let url = format!("/api/v1/nodes/{}/proxy/stats/summary", name);
+
+    let kube_request = Request::get(url).body(Default::default())?;
+    let kube_response = kube_client.request::<serde_json::Value>(kube_request).await?;
+
+    let json = json!([
+        {
+            "name": "cpu_nano_cores",
+            "metricType": "gauge",
+            "value": kube_response["node"]["cpu"]["usageNanoCores"],
+            "tags": {"hostname": name}
+        }
+    ]).to_string();
+
     let endpoint = env::var("ENDPOINT").unwrap_or("unknown".to_string());
-    let client = Client::builder().timeout(Duration::from_secs(30)).build()?;
+    let reqwest_client = Client::builder().timeout(Duration::from_secs(30)).build()?;
 
-    let json = json!({
-        "name": "cpu_nano_cores",
-        "metricType": "gauge",
-        "value": 184417755,
-        "tags": {"hostname": "minikube"}
-    }).to_string();
-
-    let res = client
+    let appsignal_response = reqwest_client
         .post(&endpoint)
         .body(json.as_str().to_owned())
         .send()
         .await?;
 
-    println!("Done: {:?}", res);
+    println!("Done: {:?}", appsignal_response);
 
     Ok(())
 }
